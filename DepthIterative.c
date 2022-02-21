dc8c143dad332329a17097a731c3310d6dbeaf90
AJ Junaidi <ameerul.junaidi@gmail.com>
2022-02-21 10:20:07 -0500

    Cleaned up codes.

--- DepthIterative.c
+++ DepthIterative.c
@@ -26,40 +26,29 @@ void solve(int ori[SIZE][SIZE], int map[SIZE][SIZE], int x, int y)
     map[x][y] = 1;          // Flip the current coordinate in the mapping to true
 
     int queue[SIZE * SIZE][2]; // Initialize the queue
-    for (int i = 0; i < SIZE * SIZE; ++i)
-        for (int j = 0; j < 2; ++j)
-            queue[i][j] = 0;
+    for (int i = 0; i < SIZE * SIZE; ++i) for (int j = 0; j < 2; ++j) queue[i][j] = 0;
 
-    int queue_index = 0, queue_size = 1; // To keep track of current index of queue and the size of queue
-    queue[queue_index][0] = x;           // Initialize first in the queue to be current x-coordinate
-    queue[queue_index][1] = y;           // Initialize first in the queue to be current y-coordinate
+    int queue_index = 0, queue_size = 1;                   // To keep track of current index and the size of queue
+    queue[queue_index][0] = x, queue[queue_index][1] = y;  // Initialize first in the queue to be the given coordinate
 
-    while (queue_size)  // Loop wil run while there is something in the queue
+    while (queue_size)
     {
-        int flipped = 0; // Variable to keep track if there is a cell in mapping that is flipped
-
         // Loop through the whole board (all rows and columns)
         // For every (current) cell, we are checking if it is adjacent to the check cell (the latest x, y in queue)
         // If it neighbors check cell, and is equal to number, and the mapping at that coordinate is 0
-        // then mark that location i, j on the map as 1, add it to the queue, and re-run the loop
-        // with the check cell as i, j
+        // then mark that location i, j on the map as 1, add it to the queue, and re-run the loop (reset i & j to zero)
+        // with the check cell as the newly added to queue i, j coordinate
+        // If we have gone through whole board, and nothing is flipped
+        // It means we have completed the check cell and need to remove the latest check cell in queue
 
         for (int i = 0; i < SIZE; ++i)
         {
             for (int j = 0; j < SIZE; ++j)
             {
-                // Get the check cell coordinate
-                int curr_x = queue[queue_index][0];
-                int curr_y = queue[queue_index][1];
+                if (ori[i][j] != number || map[i][j]) continue; // If current cell != number, and already visited, skip
 
-                if (ori[i][j] != number) continue;        // If current cell is not equal to number, skip
-                if (map[i][j]) continue;                  // If already visited (mapping already flipped), skip
-                if (i == curr_x && j == curr_y) continue; // If it is the check cell, skip
-
-
-                // Check if current cell is neighboring check cell
-                // up_check, down_check, right_check, and left_check would be true if the current cell is neighboring
-                // the check cell, which is the latest cell (coordinate x and y) added to the queue
+                int curr_x = queue[queue_index][0], curr_y = queue[queue_index][1]; // Get the check cell coordinate
+                if (i == curr_x && j == curr_y) continue;                           // If it is the check cell, skip
 
                 int up_x = (i - 1 >= 0 && i - 1 < SIZE) ? i - 1 : -1;
                 int down_x = (i + 1 >= 0 && i + 1 < SIZE) ? i + 1 : -1;
@@ -71,35 +60,22 @@ void solve(int ori[SIZE][SIZE], int map[SIZE][SIZE], int x, int y)
                 int right_check = (right_y != -1) ? i == curr_x && right_y == curr_y : 0;
                 int left_check = (left_y != -1) ? i == curr_x && left_y == curr_y : 0;
 
-                // Store checkers in an array
-                // Loop checkers, if any checker is true, then add current cell to queue
-                int checkers[4];
-                checkers[0] = up_check, checkers[1] = down_check, checkers[2] = right_check, checkers[3] = left_check;
+                if (!up_check && !down_check && !right_check && !left_check) continue; // Skip if not neighboring
 
-                for (int m = 0; m < 4; ++m)
-                {
-                    if (!checkers[m]) continue;
+                map[i][j] = 1;               // Flip mapping to 1
+                ++queue_index, ++queue_size; // Increment trackers
+                queue[queue_index][0] = i;   // Add x-coordinate of current cell to queue
+                queue[queue_index][1] = j;   // Add y-coordinate of current cell to queue
+                i = 0, j = 0;                // Restart loop, same as "break" here for inner loop
 
-                    map[i][j] = 1;               // Flip mapping to 1
-                    ++queue_index, ++queue_size; // Increment trackers
-                    queue[queue_index][0] = i;   // Add x-coordinate of current cell to queue
-                    queue[queue_index][1] = j;   // Add y-coordinate of current cell to queue
-                    flipped = 1, i = 0, j = 0;   // Restart loop, flipped to true
-                    break;                       // We only need one of the checkers to be true
-                }
-            }
-        }
+            } // Inner loop, column
+        } // Outer loop, row
 
-        // If we have gone through whole board, and nothing is flipped
-        // It means we have completed the check cell and need to remove the latest check cell in queue
+        queue[queue_index][0] = 0;   // Reset it back to zero
+        queue[queue_index][1] = 0;   // Reset it back to zero
+        --queue_index, --queue_size; // Decrement Trackers
 
-        if (!flipped)
-        {
-            queue[queue_index][0] = 0;   // Reset it back to zero
-            queue[queue_index][1] = 0;   // Reset it back to zero
-            --queue_index, --queue_size; // Decrement Trackers
-        }
-    }
+    } // Outer while loop, loop while queue_size > 0
 }
 
 int main()
@@ -118,9 +94,7 @@ int main()
     start = clock();
 
     int map[SIZE][SIZE];
-    for (int i = 0; i < SIZE; ++i)
-        for (int j = 0; j < SIZE; ++j)
-            map[i][j] = 0;
+    for (int i = 0; i < SIZE; ++i) for (int j = 0; j < SIZE; ++j) map[i][j] = 0;
 
     solve(arr, map, 1, 5);
     print_map(map);
